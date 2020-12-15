a9350b1336fc91175aa890541f199debc3142ffb
y-h-Lin <d03945002@ntu.edu.tw>
2020-12-15 14:02:22 +0800

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -27,7 +27,7 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -40,7 +40,7 @@ $(function() {
                     //clear all fields
                     $('#contact-form').trigger("reset");
                 },
-                error: function() {
+                error: function(data) {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
